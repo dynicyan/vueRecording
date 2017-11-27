b11478e458bd709a94955ee83583df38120a5eb0
dynicyan <dynicyan@qq.com>
2017-11-27 20:36:44 +0800

    add

--- src/components/vMove.vue
+++ src/components/vMove.vue
@@ -1,8 +1,11 @@
 <template lang='pug'>
-  .movePic
-    .imgContainer
-      img(:src="imgSrc")
-    .imgLiner(:style="{left: leftValue + 'px', top: topValue + 'px'}" v-if='showFlag')
+  #moveRythm
+    .movePic
+      .imgContainer
+        img(:src="imgSrc")
+      .imgLiner(:style="{left: leftValue + 'px', top: topValue + 'px'}" v-if='showFlag')
+    audio(id="myaudio" :src="videoSrc" controls="controls" loop="false" hidden="true")
+    input.playbtn(type="button" @click="play()" value="播放")
 </template>
 <script>
 export default {
@@ -10,33 +13,47 @@ export default {
   data () {
     return {
       imgSrc: require('../assets/img/iCard.jpg'),
+      videoSrc: require('../medias/paomo.mp3'),
       leftValue: 50,
       topValue: 85,
       moveTimer: '',
-      showFlag:true
+      showFlag:true,
+      isplaying: false
     }
   },
   created () {
-    let img = new Image()
-    img.src = this.imgSrc
-    console.log(img.width, img.height)
-    this.moveTimer = setInterval(() => {
-      this.leftValue++
-      if (this.leftValue > (img.width - 50)) {
-        this.leftValue = 50
-        this.topValue = this.topValue + 180
-        this.leftValue ++
-        if (this.topValue > (img.height - 85)) {
-          clearInterval(this.moveTimer)
-          this.showFlag = false
-          this.leftValue = 50
-          this.topValue = 85
-        }
-      }
-    }, 10)
   },
   methods: {
-
+    moveFun () {
+      let img = new Image()
+      img.src = this.imgSrc
+      this.moveTimer = setInterval(() => {
+        this.leftValue++
+        if (this.leftValue > (img.width - 50)) {
+          this.leftValue = 50
+          this.topValue = this.topValue + 180
+          this.leftValue ++
+          if (this.topValue > (img.height - 85)) {
+            clearInterval(this.moveTimer)
+            this.showFlag = false
+            this.leftValue = 50
+            this.topValue = 85
+          }
+        }
+      }, 100)
+    },
+    play(){
+        var audio =document.querySelector('#myaudio')
+        if(!this.isPlaying){
+          audio.play()
+          this.isPlaying = true
+          this.moveFun()
+        } else {
+          clearInterval(this.moveTimer)
+          audio.pause()
+          this.isPlaying = false
+        }
+    },
   },
   mounted () {
 
@@ -48,8 +65,9 @@ export default {
 <style lang="stylus">
 .movePic
     width 500px
-    height 667px
+    height 660px
     position relative
+    overflow: hidden;
 .imgContainer
     position absolute
     top 0
@@ -61,4 +79,9 @@ export default {
     height 150px
     position absolute
     background-color #ff3b00
+.playbtn
+    width 100px
+    height 48px
+    line-height 18px
+    background-color #ffe600
 </style>
